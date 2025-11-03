9d86ea346f7cb4bdd7b3bfa0a96feb8cd5b95683
Yunus Emre <yunuspider@gmail.com>
2025-11-03 13:42:08 +0300

    Update Decap config for Vercel OAuth

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import decapCmsOauth from 'astro-decap-cms-oauth';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), decapCmsOauth()],
 });
